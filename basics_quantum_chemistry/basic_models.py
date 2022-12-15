21b8974fce4cde76e1334b3a5ed1e4e16e5880d3
cjteclab <cjteclab@gmail.com>
2022-12-15 06:48:15 +0100

    add basic_models.py and begin coding

---
+++ basics_quantum_chemistry/basic_models.py
@@ -0,0 +1,19 @@
+"""Basic Models in Qunatum Chemistry
+
+This module contains different basic models of quantum mechanics and quantum
+chemistry. 
+"""
+
+def convert_unit(target_unit: str, number: float, unit: str) -> float:
+    pass
+
+class ParticleBox():
+    def __init__(self, length: float, mass=0.0: float):
+        self.length = length
+        self.mass = mass
+
+    def calc_n_eigenvalues(self, n=1: int):
+        # return DictComprehension
+        pass
+
+
